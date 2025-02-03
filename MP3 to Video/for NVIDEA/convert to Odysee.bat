d8e899b0d135590953f4cc466892f0a2c6f59d05
xuan69 <xuan@xuan.co.uk>
2025-02-03 17:14:45 +0000

    Create convert to Odysee.bat

---
+++ MP3 to Video/for NVIDEA/convert to Odysee.bat
@@ -0,0 +1,16 @@
+@echo off
+setlocal enabledelayedexpansion
+
+rem Set the image file name
+set image=select.jpg
+
+rem Loop over all mp3 files in the directory
+for %%f in (*.mp3) do (
+    rem Get the base file name without the extension
+    set "filename=%%~nf"
+    
+    rem Run the ffmpeg command with the current mp3 file
+    ffmpeg -loop 1 -i "!image!" -i "%%f" -c:v h264_nvenc -preset fast -pix_fmt yuv420p -maxrate 5000K -bufsize 5000K -vf "scale=if(gte(iw\,ih)\,min(1920\,iw)\,-2):if(lt(iw\,ih)\,min(1920\,ih)\,-2)" -movflags +faststart -c:a aac -b:a 160k -shortest "!filename!.mp4"
+)
+
+endlocal
